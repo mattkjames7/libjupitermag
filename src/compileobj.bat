54117a5cd6fc59d8d37ba7209288a3cae7ac2a9a
Matt <mattkjames7@gmail.com>
2023-03-07 14:04:13 +0000

    new dll

--- src/compileobj.bat
+++ src/compileobj.bat
@@ -3,3 +3,5 @@ mkdir ..\build
 g++ -fPIC -c -lm -std=c++17 -Wextra -O3 model.cc -o ..\build\model.o
 g++ -fPIC -c -lm -std=c++17 -Wextra -O3 trace.cc -o ..\build\trace.o
 g++ -fPIC -c -lm -std=c++17 -Wextra -O3 interptraceclosestpos.cc -o ..\build\interptraceclosestpos.o
+g++ -fPIC -c -lm -std=c++17 -Wextra -O3 footprint.cc -o ..\build\footprint.o
+g++ -fPIC -c -lm -std=c++17 -Wextra -O3 coordconv.cc -o ..\build\coordconv.o
