aa9db3c0d79ab24e6a53a8ad32f163fd10982cc0
matt <mattkjames7@gmail.com>
2024-01-17 00:28:43 +0000

    updated batch files

--- src/compile.bat
+++ src/compile.bat
@@ -1,3 +1,8 @@
+g++ -lm -fPIC -std=c++17 -Wextra -O3 trimstring.cc generateheader.cc -o generateheader.exe
+call generateheader.exe
+del /F /Q generateheader.exe
+
+
 call compileobj.bat
 
 g++ -fPIC -lm -std=c++17 -Wextra -O3 -shared libjupitermag.cc ..\build\*.o ..\lib\libcon2020\build\*.o ..\lib\libspline\build\*.o ..\lib\libinternalfield\build\*.o -o ..\lib\libjupitermag.dll
