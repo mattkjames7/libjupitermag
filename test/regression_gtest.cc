27c28045125edab38076b5da4a0b3c44d1ef634f
matt <mattkjames7@gmail.com>
2026-04-01 00:14:15 +0100

    more tests

--- test/regression_gtest.cc
+++ test/regression_gtest.cc
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 
+#include <array>
 #include <cmath>
 #include <cstdlib>
 #include <filesystem>
@@ -18,6 +19,15 @@ void GetInternalCFG(char *Model, bool *CartIn, bool *CartOut, int *MaxDeg);
 
 void Con2020Field(double p0, double p1, double p2, double *B0, double *B1,
                   double *B2);
+void Con2020FieldArray(int n, double *p0, double *p1, double *p2, double *B0,
+                       double *B1, double *B2);
+void GetCon2020Params(double *mui, double *irho, double *r0, double *r1,
+                      double *d, double *xt, double *xp, char *eqtype,
+                      bool *Edwards, bool *ErrChk, bool *CartIn,
+                      bool *CartOut, bool *smooth, double *DeltaRho,
+                      double *DeltaZ, double *g, char *azfunc,
+                      double *wO_open, double *wO_om, double *thetamm,
+                      double *dthetamm, double *thetaoc, double *dthetaoc);
 void SetCon2020Params(double mui, double irho, double r0, double r1, double d,
                       double xt, double xp, const char *eqtype, bool Edwards,
                       bool ErrChk, bool CartIn, bool CartOut, bool smooth,
@@ -29,6 +39,13 @@ void SetCon2020Params(double mui, double irho, double r0, double r1, double d,
 void ModelField(double p0, double p1, double p2, const char *internal,
                 const char *external, bool CartIn, bool CartOut, double *B0,
                 double *B1, double *B2);
+void ModelFieldArray(int n, double *p0, double *p1, double *p2,
+                     const char *internal, const char *external, bool CartIn,
+                     bool CartOut, double *B0, double *B1, double *B2);
+
+typedef void (*modelFieldPtr)(double, double, double, double *, double *,
+                              double *);
+modelFieldPtr getModelFieldPtr(const char *Model);
 
 bool TraceField(int n, double *x0, double *y0, double *z0, const char *IntFunc,
                 int nExt, char **ExtFunc, int MaxLen, double MaxStep,
@@ -117,7 +134,213 @@ std::filesystem::path TestDataDir() {
     return cwd / "data";
 }
 
+std::vector<std::array<double, 3>> RegressionPoints() {
+    return {
+        {1.5, 0.1, -0.2},
+        {5.0, 0.0, 0.0},
+        {10.0, 2.0, -1.0},
+        {20.0, -10.0, 3.0},
+        {30.0, 0.0, 15.0},
+        {40.0, 5.0, -20.0},
+    };
+}
+
 }  // namespace
+
+TEST(Regressions, InternalConfigRoundTrip) {
+    ConfigureModelsForBaseline();
+
+    char model[64] = {0};
+    bool cartIn = false;
+    bool cartOut = false;
+    int maxDeg = 0;
+
+    GetInternalCFG(model, &cartIn, &cartOut, &maxDeg);
+    const int requestedMaxDeg = (maxDeg > 3) ? (maxDeg - 2) : maxDeg;
+
+    SetInternalCFG("jrm33", false, true, requestedMaxDeg);
+
+    char gotModel[64] = {0};
+    bool gotCartIn = true;
+    bool gotCartOut = false;
+    int gotMaxDeg = 0;
+    GetInternalCFG(gotModel, &gotCartIn, &gotCartOut, &gotMaxDeg);
+
+    EXPECT_STREQ("jrm33", gotModel);
+    EXPECT_FALSE(gotCartIn);
+    EXPECT_TRUE(gotCartOut);
+    EXPECT_EQ(requestedMaxDeg, gotMaxDeg);
+
+    SetInternalCFG("jrm33", true, true, maxDeg);
+}
+
+TEST(Regressions, Con2020ConfigRoundTrip) {
+    ConfigureModelsForBaseline();
+
+    const double mui = 140.5;
+    const double irho = 17.2;
+    const double r0 = 8.1;
+    const double r1 = 49.7;
+    const double d = 3.5;
+    const double xt = 8.9 * kDeg2Rad;
+    const double xp = 158.2 * kDeg2Rad;
+    const bool edwards = false;
+    const bool errChk = true;
+    const bool cartIn = false;
+    const bool cartOut = true;
+    const bool smooth = true;
+    const double deltaRho = 0.9;
+    const double deltaZ = 0.12;
+    const double g = 417000.0;
+    const double wOOpen = 0.2;
+    const double wOOm = 0.4;
+    const double thetamm = 15.8 * kDeg2Rad;
+    const double dthetamm = 0.6 * kDeg2Rad;
+    const double thetaoc = 10.9 * kDeg2Rad;
+    const double dthetaoc = 0.15 * kDeg2Rad;
+
+    SetCon2020Params(mui, irho, r0, r1, d, xt, xp, "integral", edwards, errChk,
+                     cartIn, cartOut, smooth, deltaRho, deltaZ, g, "lmic",
+                     wOOpen, wOOm, thetamm, dthetamm, thetaoc, dthetaoc);
+
+    double gotMui = 0.0, gotIrho = 0.0, gotR0 = 0.0, gotR1 = 0.0, gotD = 0.0;
+    double gotXt = 0.0, gotXp = 0.0;
+    char gotEqtype[32] = {0};
+    bool gotEdwards = true, gotErrChk = false, gotCartIn = true,
+         gotCartOut = false, gotSmooth = false;
+    double gotDeltaRho = 0.0, gotDeltaZ = 0.0, gotG = 0.0;
+    char gotAzfunc[32] = {0};
+    double gotWOOpen = 0.0, gotWOOm = 0.0, gotThetaMM = 0.0, gotDThetaMM = 0.0,
+           gotThetaOC = 0.0, gotDThetaOC = 0.0;
+
+    GetCon2020Params(&gotMui, &gotIrho, &gotR0, &gotR1, &gotD, &gotXt, &gotXp,
+                     gotEqtype, &gotEdwards, &gotErrChk, &gotCartIn,
+                     &gotCartOut, &gotSmooth, &gotDeltaRho, &gotDeltaZ, &gotG,
+                     gotAzfunc, &gotWOOpen, &gotWOOm, &gotThetaMM, &gotDThetaMM,
+                     &gotThetaOC, &gotDThetaOC);
+
+    EXPECT_TRUE(NearlyEqual(mui, gotMui, kFieldAbsTol, kFieldRelTol));
+    EXPECT_TRUE(NearlyEqual(irho, gotIrho, kFieldAbsTol, kFieldRelTol));
+    EXPECT_TRUE(NearlyEqual(r0, gotR0, kFieldAbsTol, kFieldRelTol));
+    EXPECT_TRUE(NearlyEqual(r1, gotR1, kFieldAbsTol, kFieldRelTol));
+    EXPECT_TRUE(NearlyEqual(d, gotD, kFieldAbsTol, kFieldRelTol));
+    EXPECT_TRUE(NearlyEqual(xt, gotXt, kFieldAbsTol, kFieldRelTol));
+    EXPECT_TRUE(NearlyEqual(xp, gotXp, kFieldAbsTol, kFieldRelTol));
+    EXPECT_STREQ("integral", gotEqtype);
+    EXPECT_EQ(edwards, gotEdwards);
+    EXPECT_EQ(errChk, gotErrChk);
+    EXPECT_EQ(cartIn, gotCartIn);
+    EXPECT_EQ(cartOut, gotCartOut);
+    EXPECT_EQ(smooth, gotSmooth);
+    EXPECT_TRUE(NearlyEqual(deltaRho, gotDeltaRho, kFieldAbsTol, kFieldRelTol));
+    EXPECT_TRUE(NearlyEqual(deltaZ, gotDeltaZ, kFieldAbsTol, kFieldRelTol));
+    EXPECT_TRUE(NearlyEqual(g, gotG, kFieldAbsTol, kFieldRelTol));
+    EXPECT_STREQ("lmic", gotAzfunc);
+    EXPECT_TRUE(NearlyEqual(wOOpen, gotWOOpen, kFieldAbsTol, kFieldRelTol));
+    EXPECT_TRUE(NearlyEqual(wOOm, gotWOOm, kFieldAbsTol, kFieldRelTol));
+    EXPECT_TRUE(NearlyEqual(thetamm, gotThetaMM, kFieldAbsTol, kFieldRelTol));
+    EXPECT_TRUE(
+        NearlyEqual(dthetamm, gotDThetaMM, kFieldAbsTol, kFieldRelTol));
+    EXPECT_TRUE(NearlyEqual(thetaoc, gotThetaOC, kFieldAbsTol, kFieldRelTol));
+    EXPECT_TRUE(
+        NearlyEqual(dthetaoc, gotDThetaOC, kFieldAbsTol, kFieldRelTol));
+
+    ConfigureModelsForBaseline();
+}
+
+TEST(Regressions, ScalarArrayConsistency) {
+    ConfigureModelsForBaseline();
+
+    const auto points = RegressionPoints();
+    const int n = static_cast<int>(points.size());
+    std::vector<double> p0(n), p1(n), p2(n);
+    std::vector<double> iArr0(n), iArr1(n), iArr2(n);
+    std::vector<double> eArr0(n), eArr1(n), eArr2(n);
+    std::vector<double> mArr0(n), mArr1(n), mArr2(n);
+
+    for (int i = 0; i < n; i++) {
+        p0[i] = points[i][0];
+        p1[i] = points[i][1];
+        p2[i] = points[i][2];
+    }
+
+    InternalField(n, p0.data(), p1.data(), p2.data(), iArr0.data(), iArr1.data(),
+                  iArr2.data());
+    Con2020FieldArray(n, p0.data(), p1.data(), p2.data(), eArr0.data(),
+                      eArr1.data(), eArr2.data());
+    ModelFieldArray(n, p0.data(), p1.data(), p2.data(), "jrm33", "Con2020",
+                    true, true, mArr0.data(), mArr1.data(), mArr2.data());
+
+    modelFieldPtr internalScalar = getModelFieldPtr("jrm33");
+    ASSERT_NE(nullptr, internalScalar);
+
+    for (int i = 0; i < n; i++) {
+        double is0 = 0.0, is1 = 0.0, is2 = 0.0;
+        double es0 = 0.0, es1 = 0.0, es2 = 0.0;
+        double ms0 = 0.0, ms1 = 0.0, ms2 = 0.0;
+
+        internalScalar(p0[i], p1[i], p2[i], &is0, &is1, &is2);
+        Con2020Field(p0[i], p1[i], p2[i], &es0, &es1, &es2);
+        ModelField(p0[i], p1[i], p2[i], "jrm33", "Con2020", true, true, &ms0,
+                   &ms1, &ms2);
+
+        EXPECT_TRUE(NearlyEqual(is0, iArr0[i], kFieldAbsTol, kFieldRelTol)) << i;
+        EXPECT_TRUE(NearlyEqual(is1, iArr1[i], kFieldAbsTol, kFieldRelTol)) << i;
+        EXPECT_TRUE(NearlyEqual(is2, iArr2[i], kFieldAbsTol, kFieldRelTol)) << i;
+
+        EXPECT_TRUE(NearlyEqual(es0, eArr0[i], kFieldAbsTol, kFieldRelTol)) << i;
+        EXPECT_TRUE(NearlyEqual(es1, eArr1[i], kFieldAbsTol, kFieldRelTol)) << i;
+        EXPECT_TRUE(NearlyEqual(es2, eArr2[i], kFieldAbsTol, kFieldRelTol)) << i;
+
+        EXPECT_TRUE(NearlyEqual(ms0, mArr0[i], kFieldAbsTol, kFieldRelTol)) << i;
+        EXPECT_TRUE(NearlyEqual(ms1, mArr1[i], kFieldAbsTol, kFieldRelTol)) << i;
+        EXPECT_TRUE(NearlyEqual(ms2, mArr2[i], kFieldAbsTol, kFieldRelTol)) << i;
+    }
+}
+
+TEST(Regressions, ModelCompositionIdentity) {
+    ConfigureModelsForBaseline();
+
+    const auto points = RegressionPoints();
+    for (size_t i = 0; i < points.size(); i++) {
+        const double x = points[i][0];
+        const double y = points[i][1];
+        const double z = points[i][2];
+
+        double bi0 = 0.0, bi1 = 0.0, bi2 = 0.0;
+        double be0 = 0.0, be1 = 0.0, be2 = 0.0;
+        double bc0 = 0.0, bc1 = 0.0, bc2 = 0.0;
+        double boi0 = 0.0, boi1 = 0.0, boi2 = 0.0;
+        double boe0 = 0.0, boe1 = 0.0, boe2 = 0.0;
+
+        double xx = x, yy = y, zz = z;
+        InternalField(1, &xx, &yy, &zz, &bi0, &bi1, &bi2);
+        Con2020Field(x, y, z, &be0, &be1, &be2);
+        ModelField(x, y, z, "jrm33", "Con2020", true, true, &bc0, &bc1, &bc2);
+
+        ModelField(x, y, z, "jrm33", "none", true, true, &boi0, &boi1, &boi2);
+        ModelField(x, y, z, "none", "Con2020", true, true, &boe0, &boe1,
+                   &boe2);
+
+        EXPECT_TRUE(
+            NearlyEqual(bi0 + be0, bc0, 10.0 * kFieldAbsTol, 10.0 * kFieldRelTol))
+            << i;
+        EXPECT_TRUE(
+            NearlyEqual(bi1 + be1, bc1, 10.0 * kFieldAbsTol, 10.0 * kFieldRelTol))
+            << i;
+        EXPECT_TRUE(
+            NearlyEqual(bi2 + be2, bc2, 10.0 * kFieldAbsTol, 10.0 * kFieldRelTol))
+            << i;
+
+        EXPECT_TRUE(NearlyEqual(bi0, boi0, kFieldAbsTol, kFieldRelTol)) << i;
+        EXPECT_TRUE(NearlyEqual(bi1, boi1, kFieldAbsTol, kFieldRelTol)) << i;
+        EXPECT_TRUE(NearlyEqual(bi2, boi2, kFieldAbsTol, kFieldRelTol)) << i;
+
+        EXPECT_TRUE(NearlyEqual(be0, boe0, kFieldAbsTol, kFieldRelTol)) << i;
+        EXPECT_TRUE(NearlyEqual(be1, boe1, kFieldAbsTol, kFieldRelTol)) << i;
+        EXPECT_TRUE(NearlyEqual(be2, boe2, kFieldAbsTol, kFieldRelTol)) << i;
+    }
+}
 
 TEST(Regressions, FieldBaselineCSV) {
     ConfigureModelsForBaseline();
