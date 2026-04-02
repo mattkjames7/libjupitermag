45073fa5150302218d8c225959f3401bf8679595
matt <mattkjames7@gmail.com>
2026-04-02 23:14:26 +0100

    fix duplicate definition

--- test/regression_gtest.cc
+++ test/regression_gtest.cc
@@ -10,59 +10,7 @@
 #include <sstream>
 #include <string>
 #include <vector>
-
-extern "C" {
-
-void InternalField(int n, double *p0, double *p1, double *p2, double *B0,
-                   double *B1, double *B2);
-void SetInternalCFG(const char *Model, bool CartIn, bool CartOut, int MaxDeg);
-void GetInternalCFG(char *Model, bool *CartIn, bool *CartOut, int *MaxDeg);
-
-void Con2020Field(double p0, double p1, double p2, double *B0, double *B1,
-                  double *B2);
-void Con2020FieldArray(int n, double *p0, double *p1, double *p2, double *B0,
-                       double *B1, double *B2);
-void GetCon2020Params(double *mui, double *irho, double *r0, double *r1,
-                      double *d, double *xt, double *xp, char *eqtype,
-                      bool *Edwards, bool *ErrChk, bool *CartIn,
-                      bool *CartOut, bool *smooth, double *DeltaRho,
-                      double *DeltaZ, double *g, char *azfunc,
-                      double *wO_open, double *wO_om, double *thetamm,
-                      double *dthetamm, double *thetaoc, double *dthetaoc);
-void SetCon2020Params(double mui, double irho, double r0, double r1, double d,
-                      double xt, double xp, const char *eqtype, bool Edwards,
-                      bool ErrChk, bool CartIn, bool CartOut, bool smooth,
-                      double DeltaRho, double DeltaZ, double g,
-                      const char *azfunc, double wO_open, double wO_om,
-                      double thetamm, double dthetamm, double thetaoc,
-                      double dthetaoc);
-
-void ModelField(double p0, double p1, double p2, const char *internal,
-                const char *external, bool CartIn, bool CartOut, double *B0,
-                double *B1, double *B2);
-void ModelFieldArray(int n, double *p0, double *p1, double *p2,
-                     const char *internal, const char *external, bool CartIn,
-                     bool CartOut, double *B0, double *B1, double *B2);
-
-typedef void (*modelFieldPtr)(double, double, double, double *, double *,
-                              double *);
-modelFieldPtr getModelFieldPtr(const char *Model);
-
-bool TraceField(int n, double *x0, double *y0, double *z0, const char *IntFunc,
-                int nExt, char **ExtFunc, int MaxLen, double MaxStep,
-                double InitStep, double MinStep, double ErrMax, double Delta,
-                bool Verbose, int TraceDir, double as, double bs, double ai,
-                double bi, int *nstep, double **x, double **y, double **z,
-                double **Bx, double **By, double **Bz, double **R, double **S,
-                double **Rnorm, int **traceRegion, double **FP, int nalpha,
-                double *alpha, double *halpha);
-}
-
-void SIIItoMag(double x3, double y3, double z3, double xt, double xp,
-               double *xm, double *ym, double *zm);
-void MagtoSIII(double xm, double ym, double zm, double xt, double xp,
-               double *x3, double *y3, double *z3);
-double planetRadius(double x, double y, double z, double a, double b);
+#include "jupitermag.h"
 
 namespace {
 
