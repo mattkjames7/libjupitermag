1350068425f0b343025899a7e11d6d8d295b082f
matt <mattkjames7@gmail.com>
2026-04-08 22:35:03 +0100

    pi

--- test/unit/regression_common.h
+++ test/unit/regression_common.h
@@ -8,6 +8,10 @@
 
 #include "jupitermag.h"
 
+#ifndef M_PI
+    #define M_PI 3.14159265358979323846
+#endif
+
 inline constexpr double kDeg2Rad = M_PI / 180.0;
 inline constexpr double kFieldAbsTol = 1e-6;
 inline constexpr double kFieldRelTol = 1e-6;
